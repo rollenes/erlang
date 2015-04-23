72a3c5f349ae2af3f52559749c57115184efac41
rollen <rafal.wartalski@gmail.com>
2015-04-23 22:11:04 +0200

    fib tests in more compact notation

--- fib.erl
+++ fib.erl
@@ -1,6 +1,5 @@
 -module(fib).
 -include_lib("eunit/include/eunit.hrl").
--export([fib/1]).
 
 fib (0) ->
     1;
@@ -10,9 +9,11 @@ fib (X) ->
     fib(X - 1) + fib(X - 2).
 
 
-fib_0_test() -> 1 = fib(0).
-fib_1_test() -> 1 = fib(1).
-fib_2_test() -> 2 = fib(2).
-fib_3_test() -> 3 = fib(3).
-fib_4_test() -> 5 = fib(4).
-
+fib_test_() ->
+    [
+        ?_assertEqual(1, fib(0)),
+        ?_assertEqual(1, fib(1)),
+        ?_assertEqual(2, fib(2)),
+        ?_assertEqual(3, fib(3)),
+        ?_assertEqual(5, fib(4))
+    ].
